8fd56025448a1011bbd48bd13861fb4a5b4bc4c9
Mahmudul Hasan Sabbir <32410301+SA33IR@users.noreply.github.com>
2018-05-29 22:39:16 +0600

    Initial Commit

---
+++ Pendulum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    float timer = 0f;
+    float speed = 1f;
+    int phase = 0;
+    void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+        if(timer>1f)
+        {
+            phase++;
+            phase %= 4;            //Keep the phase between 0 to 3.
+            timer = 0f;
+        }
+
+        switch(phase)
+        {
+            case 0:
+                transform.Rotate(0f, 0f, speed * (1 - timer));  //Speed, from maximum to zero.
+                break;
+            case 1:
+                transform.Rotate(0f, 0f, -speed * timer);       //Speed, from zero to maximum.
+                break;
+            case 2:
+                transform.Rotate(0f, 0f, -speed * (1 - timer)); //Speed, from maximum to zero.
+                break;
+            case 3:
+                transform.Rotate(0f, 0f, speed * timer);        //Speed, from zero to maximum.
+                break;
+        }
+    }
+}
